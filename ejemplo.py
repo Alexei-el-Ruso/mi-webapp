64c02931a2be3873b95e178c00d012d197861a3b
Alexei-el-Ruso <alexeiromamx@gmail.com>
2025-04-04 12:51:44 -0600

    main

---
+++ ejemplo.py
@@ -0,0 +1,18 @@
+"""
+Cristian Alexei Romero Martinez
+Código de Python
+Ingeniería en Sistemas Computacionales
+
+Servidor Web
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
